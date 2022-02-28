73ab36ffae0b18b13bf35c1a2b3f853a03825d26
mohamedarief07 <100037273+mohamedarief07@users.noreply.github.com>
2022-02-28 23:48:23 +0530

    Add files via upload

---
+++ DeleteLead.java
@@ -0,0 +1,48 @@
+package week2.day2.assignments;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DeleteLead {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("Demosalesmanager");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("crmsfa");
+		driver.findElement(By.xpath("//input[@class='decorativeSubmit']")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'CRM/SFA')]")).click();
+		driver.findElement(By.xpath("//a[text()='Leads']")).click();
+		driver.findElement(By.xpath("//a[text()='Find Leads']")).click();
+		driver.findElement(By.xpath("(//span[@class='x-tab-strip-text '])[2]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//input[@name='phoneAreaCode'])")).sendKeys("602");
+		driver.findElement(By.xpath("(//button[@class='x-btn-text'])[7]")).click();
+		Thread.sleep(5000);
+		String text = driver.findElement(By.xpath("(//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a)[1]")).getText();
+		System.out.println("Capture lead ID of First Resulting lead is :" +text);	
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a)[1]")).click();
+		driver.findElement(By.linkText("Delete")).click();
+		driver.findElement(By.xpath("//a[text()='Find Leads']")).click();
+		driver.findElement(By.xpath("(//div[@class='x-form-element'])[18]/input")).sendKeys(text);
+		driver.findElement(By.xpath("(//button[@class='x-btn-text'])[7]")).click();
+		Thread.sleep(2000);
+		String verifymessage = driver.findElement(By.xpath("//div[@class='x-paging-info']")).getText();
+		System.out.println("This message confirms the successful deletion :" +verifymessage);	
+		
+		if(verifymessage.contains("No records to display")) {
+			System.out.println("This case is PASSED");
+		}else
+		{
+			System.out.println("This case is FAILED");
+		}
+		driver.close();	
+	
+	}
+}
